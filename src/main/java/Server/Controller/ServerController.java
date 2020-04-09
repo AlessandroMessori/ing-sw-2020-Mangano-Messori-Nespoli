6d3896a43deca9c560100173fff596ea90ae3a10
Davide Mangano <gorgeousdeformity@gmail.com>
2020-04-09 20:58:28 +0200

    calculateNextMove() partially developed. Added some variables and modified some functions  in Game and ClientController.

--- src/main/java/Server/Controller/ServerController.java
+++ src/main/java/Server/Controller/ServerController.java
@@ -35,25 +35,28 @@ public class ServerController {
      * adds the player to the game
      * @param p player to add
      * @param if3 boolean which checks if the player wants to play in a 2 or a 3 players game
+     * @return the Game to which the player is added
      */
-    public void addPlayerToModel(Player p, boolean if3){        //TODO SYNCHRONIZED
+    public Game addPlayerToModel(Player p, boolean if3){        //TODO SYNCHRONIZED
         String x;
         Model model = Model.getModel();
 
         if(model.getGames().size() == 0){       //starting case
             model.addGame(new Game(0,x = randomString(10),if3,null,null,null,null));
             model.searchID(x).getPlayers().addPlayer(p);
-            return;
+            return(model.searchID(x));
         }
         for(Game g : model.getGames()){
             if(!g.getThreePlayers() && !if3){           //if 2 players
                 if(g.getPlayers().size() < 2){
                     g.getPlayers().addPlayer(p);
+                    return g;
                 }
             }
             else if(g.getThreePlayers() && if3){       //if 3 players
                 if(g.getPlayers().size() < 3){
                     g.getPlayers().addPlayer(p);
+                    return g;
                 }
             }
             else {
@@ -64,8 +67,10 @@ public class ServerController {
                 }
                 model.addGame(new Game(0,x,if3,null,null,null,null));
                 model.searchID(x).getPlayers().addPlayer(p);
+                return model.searchID(x);
             }
         }
+        return null;
     }
 
     /**
@@ -98,37 +103,58 @@ public class ServerController {
      * @param grid grid on which to calculate the next possible move(s), based on the player's divinity
      * @return the possible MoveList
      */
-    public MoveList calculateNextMove(Grid grid, Player p, String gameID, Move move){
+    public MoveList calculateNextMove(Grid grid, Player p, String gameID, Move move, Turn turn){
         MoveList movelist = null;
         Model model = Model.getModel();
+        Game game = model.searchID(gameID);
 
-        Turn turn = new Turn(p.getDivinity());
-        turn.startingTurn();
+        /*Turn turn = new Turn(p.getDivinity());      //THING TO DO BEFORE CALL A INIZIO GIOCO!
+        turn.startingTurn();   */                     //THING TO DO IMMEDIATELY BEFORE CALL
 
-        if(move.getIfMove()) {
-            turn.canItComeUp(grid, move); //TODO: FIX THIS
+        if(move.getIfMove()) {              //IF MOVEMENT
+            movelist = null;
+            /*turn.canItComeUp(grid, move); //TODO: PUT OUTSIDE THIS FUNCTION, WHEN CALCULATENEXTMOVE() IS CALLED
             if (!turn.getCanComeUp()) {
-                if (p.getDivinity().equals("Prometheus")) {
+                if (turn.getCanBuildBeforeMove()) {
+                    move.setIfMove(false);
+                    calculateNextMove(grid,p,gameID,move,turn);
+                    move.setIfMove(true);
                     //TODO: BUILD BEFORE MOVE
                 }
-            }
-            if (turn.getCanSwap())       //APOLLO
-            {
+            }*/
 
-            }
             if (turn.getCanMoveAndSwap())        //MINOTAUR
             {
 
             }
 
-            while (turn.getNPossibleMoves() > 0) {
+            if (turn.getNPossibleMoves() > 0) {
                 if (grid.getCells(move.getX() + 1, move.getY()).getTower().getLevel() <= grid.getCells(move.getX(), move.getY()).getTower().getLevel() + 1) {
                     if (!grid.getCells(move.getX() + 1, move.getY()).getTower().getIsDome()) {
-                        Move possMove = new Move(move.getToMove());
+                        if((grid.getCells(move.getX() + 1, move.getY()).getPawn() == null)) {
+                            Move possMove = new Move(move.getToMove());
 
-                        possMove.setX(move.getX() + 1);
-                        possMove.setY(move.getY());
-                        movelist.addMove(possMove);
+                            possMove.setIfMove(true);
+                            possMove.setX(move.getX() + 1);
+                            possMove.setY(move.getY());
+                            movelist.addMove(possMove);
+                        }
+                        else if(turn.getCanSwap()){     //APOLLO EFFECT
+                            Move possMove = new Move(move.getToMove());
+                            Move consecMove = new Move(grid.getCells(move.getX() + 1,move.getY()).getPawn());
+                            Pawn pawn = grid.getCells(move.getX() + 1,move.getY()).getPawn();
+
+                            possMove.setIfMove(true);
+                            possMove.setX(move.getX() + 1);
+                            possMove.setY(move.getY());
+                            movelist.addMove(possMove);
+
+                            /*consecMove.setIfMove(true);     //TODO: MAKE IT IMMEDIATELY AFTER POSSMOVE
+                            consecMove.setX(move.getX());
+                            consecMove.setY(move.getY());
+                            movelist.addMove(consecMove);*/
+
+                        }
 
                     }
                 }
@@ -203,7 +229,88 @@ public class ServerController {
                     }
                 }
                 turn.setNPossibleMoves(turn.getNPossibleMoves() - 1);
+                turn.setNMovesMade(turn.getNMovesMade() + 1);
             }
+        }
+
+        else if(!move.getIfMove()){             //BUILDING MOVE
+            movelist = null;
+            if(turn.getCanBuildDomes()){        //ATLAS EFFECT
+                for(int i = 0; i <= 4; i++){
+                    for(int j = 0; i <= 4; j++){
+                        if(grid.getCells(i,j).getPawn() == null && !grid.getCells(i,j).getTower().getIsDome()){
+                            if(game.getAvailableDomes() > 0) {
+                                Move possMove = new Move(null);
+
+                                possMove.setIfMove(false);
+                                possMove.setX(i);
+                                possMove.setY(j);
+                                movelist.addMove(possMove);
+                            }
+                        }
+                    }
+
+                }
+            }
+            for(int i = -1; i <= 1; i++){
+                for(int j = -1; j <= 1; j++ ){
+                    if(grid.getCells(move.getX() + i, move.getY() + j).getPawn() == null && !grid.getCells(move.getX() + i, move.getY() + j).getTower().getIsDome()){
+                        if(grid.getCells(move.getX() + i, move.getY() + j).getTower().getLevel() == 0) {
+                            if(game.getAvailableLevel1Buildings() > 0) {
+                                Move possMove = new Move(null);
+
+                                possMove.setIfMove(false);
+                                possMove.setX(move.getX() + i);
+                                possMove.setY(move.getY() + j);
+
+                                movelist.addMove(possMove);
+
+                            }
+                        }
+                        if(grid.getCells(move.getX() + i, move.getY() + j).getTower().getLevel() == 1){
+                            if(game.getAvailableLevel2Buildings() > 0) {
+                                Move possMove = new Move(null);
+
+                                possMove.setIfMove(false);
+                                possMove.setX(move.getX() + i);
+                                possMove.setY(move.getY() + j);
+
+                                movelist.addMove(possMove);
+
+                            }
+                        }
+
+                        if(grid.getCells(move.getX() + i, move.getY() + j).getTower().getLevel() == 2){
+                            if(game.getAvailableLevel3Buildings() > 0){
+                                Move possMove = new Move(null);
+
+                                possMove.setIfMove(false);
+                                possMove.setX(move.getX() + i);
+                                possMove.setY(move.getY() + j);
+
+                                movelist.addMove(possMove);
+
+                            }
+                        }
+
+                        if(grid.getCells(move.getX() + i, move.getY() + j).getTower().getLevel() == 3){
+                            if(game.getAvailableDomes() > 0){
+                                Move possMove = new Move(null);
+
+                                possMove.setIfMove(false);
+                                possMove.setX(move.getX() + i);
+                                possMove.setY(move.getY() + j);
+
+                                movelist.addMove(possMove);
+
+                            }
+                        }
+                    }
+
+                }
+            }
+            turn.setNMadeBuildings(turn.getNMadeBuildings() + 1);
+            turn.setNPossibleBuildings(turn.getNPossibleBuildings() - 1);
         }
 
 
