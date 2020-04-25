7753a873dc0ff35ab02653e6d760e13d6fa7694f
Davide Mangano <gorgeousdeformity@gmail.com>
2020-04-26 01:44:27 +0200

    Minor fixes in ClientController.

--- src/main/java/Client/Controller/ClientController.java
+++ src/main/java/Client/Controller/ClientController.java
@@ -82,8 +82,10 @@ public class ClientController {
                                     for(int k = 0; k <= 4; k++){
                                         for(int l = 0; l <= 4; l++){
                                             if(game.getNewGrid().getCells(k,l).getPawn() != null){
-                                                if(game.getNewGrid().getCells(k,l).getPawn().getId() == game.getGameTurn().getEnemyPawn1().getId()){
-                                                    game.getGameTurn().setEnemyPawn1(null);
+                                                if(game.getGameTurn().getEnemyPawn1() != null) {
+                                                    if (game.getNewGrid().getCells(k, l).getPawn().getId() == game.getGameTurn().getEnemyPawn1().getId()) {
+                                                        game.getGameTurn().setEnemyPawn1(null);
+                                                    }
                                                 }
                                                 if(game.getGameTurn().getEnemyPawn2() != null){
                                                     if(game.getNewGrid().getCells(k,l).getPawn().getId() == game.getGameTurn().getEnemyPawn2().getId()){
