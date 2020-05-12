1ee5d3e07d030fdbe3b2b44494d29776b0aa30fb
AlessandroMessori <messori.alessandro.98@gmail.com>
2020-05-12 13:48:21 +0200

    update on GamePage

--- src/main/java/it/polimi/ingsw/PSP19/Client/GUI/GamePage.java
+++ src/main/java/it/polimi/ingsw/PSP19/Client/GUI/GamePage.java
@@ -1,5 +1,6 @@
 package it.polimi.ingsw.PSP19.Client.GUI;
 
+import it.polimi.ingsw.PSP19.Server.Model.*;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.Node;
@@ -16,35 +17,56 @@ public class GamePage extends Page implements Initializable {
     @FXML
     private GridPane gameGrid;
 
+    private boolean alreadyChosenPawn = false;
+
     public String getPageName() {
         return "Game";
     }
 
-    @FXML
-    private void printCell(MouseEvent e) {
-        Node source = (Node) e.getSource();
-        //System.out.println(e.get);
-    }
-
-    // 12
-
-    private ImageView getGameGridCell(int x, int y) {
-        return (ImageView) gameGrid.getChildren().get(x * 5 + y);
+    private ImageView getGameGridCell(int x, int y, boolean pawn) {
+        int offset = pawn ? 25 : 0;
+        return (ImageView) gameGrid.getChildren().get(offset + x * 5 + y);
     }
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
 
+        game = new Game(0, null, false, null, new Grid(), new Grid(), null);
+        game.getNewGrid().getCells(0, 3).getTower().setLevel(1);
+        game.getNewGrid().getCells(2, 1).getTower().setLevel(1);
+        game.getNewGrid().getCells(3,4).setPawn(new Pawn(new Player("G1",Divinity.ARTEMIS,Colour.RED)));
+        game.getNewGrid().getCells(2,1).setPawn(new Pawn(new Player("G1",Divinity.ARTEMIS,Colour.RED)));
+
         for (int i = 0; i < 5; i++) {
             for (int j = 0; j < 5; j++) {
                 int finalI = i;
                 int finalJ = j;
+                ImageView currentTowerImage = getGameGridCell(i, j,false);
+                ImageView currentPawnImage = getGameGridCell(i,j,true);
+                Cell currentCell = game.getNewGrid().getCells(i, j);
 
-                getGameGridCell(i,j).setOnMouseClicked(e -> {
-                    System.out.printf("Clicked at Cell %d,%d\n", finalI,finalJ);
+                currentTowerImage.setOnMouseClicked(e -> {
+                    System.out.printf("Clicked at Cell %d,%d\n", finalI, finalJ);
+
+                    /*if (alreadyChosenPawn) {
+
+                    } else {
+                        if (currentCell.getPawn().getId() == game.getCurrentPlayer().getCurrentPawn().getId()) {
+
+                        }
+                    }*/
+
                 });
 
-                getGameGridCell(i,j).setImage(new Image("/Images/button-play-normal.png"));
+                // draws Tower
+                if (currentCell.getTower().getLevel() > 0) {
+                    currentTowerImage.setImage(new Image("/Images/button-play-normal.png"));
+                }
+
+                // draws Pawn
+                if (currentCell.getPawn() != null) {
+                    currentPawnImage.setImage(new Image("/Images/logo.png"));
+                }
 
             }
         }
