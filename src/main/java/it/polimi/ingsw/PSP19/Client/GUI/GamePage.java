1593885ef8c956abc7d811e40ac866e86b3530b3
AlessandroMessori <messori.alessandro.98@gmail.com>
2020-05-13 12:25:19 +0200

    update on GamePage

--- src/main/java/it/polimi/ingsw/PSP19/Client/GUI/GamePage.java
+++ src/main/java/it/polimi/ingsw/PSP19/Client/GUI/GamePage.java
@@ -31,30 +31,46 @@ public class GamePage extends Page implements Initializable {
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
 
+        Player g1, g2;
+
+        g1 = new Player("G1", Divinity.ARTEMIS, Colour.RED);
+        g2 = new Player("G2", Divinity.DEMETER, Colour.BLUE);
+
         game = new Game(0, null, false, null, new Grid(), new Grid(), null);
+        game.getPlayers().addPlayer(g1);
+        game.getPlayers().addPlayer(g2);
         game.getNewGrid().getCells(0, 3).getTower().setLevel(1);
         game.getNewGrid().getCells(2, 1).getTower().setLevel(1);
-        game.getNewGrid().getCells(3,4).setPawn(new Pawn(new Player("G1",Divinity.ARTEMIS,Colour.RED)));
-        game.getNewGrid().getCells(2,1).setPawn(new Pawn(new Player("G1",Divinity.ARTEMIS,Colour.RED)));
+        game.getNewGrid().getCells(3, 4).setPawn(new Pawn(g1));
+        game.getNewGrid().getCells(2, 1).setPawn(new Pawn(g1));
 
         for (int i = 0; i < 5; i++) {
             for (int j = 0; j < 5; j++) {
                 int finalI = i;
                 int finalJ = j;
-                ImageView currentTowerImage = getGameGridCell(i, j,false);
-                ImageView currentPawnImage = getGameGridCell(i,j,true);
+                ImageView currentTowerImage = getGameGridCell(i, j, false);
+                ImageView currentPawnImage = getGameGridCell(i, j, true);
                 Cell currentCell = game.getNewGrid().getCells(i, j);
+                game.setCurrentPlayer(g1);
 
-                currentTowerImage.setOnMouseClicked(e -> {
+                currentPawnImage.setOnMouseClicked(e -> {
                     System.out.printf("Clicked at Cell %d,%d\n", finalI, finalJ);
 
-                    /*if (alreadyChosenPawn) {
+                    if  (currentCell.getPawn() != null) {
+                        System.out.println(currentCell.getPawn().getOwner().getUsername());
+                        System.out.println(game.getCurrentPlayer());
+                    }
 
+                    if (alreadyChosenPawn) {
+                        System.out.println("Already Chosen Pawn");
                     } else {
-                        if (currentCell.getPawn().getId() == game.getCurrentPlayer().getCurrentPawn().getId()) {
-
+                        // Selecting The Pawn for this Turn
+                        if (currentCell.getPawn() != null && currentCell.getPawn().getOwner().getUsername().equals(game.getCurrentPlayer().getUsername())) {
+                            alreadyChosenPawn = true;
+                            game.getCurrentPlayer().setCurrentPawn(currentCell.getPawn());
+                            System.out.println("Selecting Pawn");
                         }
-                    }*/
+                    }
 
                 });
 
