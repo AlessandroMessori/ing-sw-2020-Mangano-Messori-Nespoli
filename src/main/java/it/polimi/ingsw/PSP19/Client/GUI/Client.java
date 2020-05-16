eb5518963ce3a3876f3799f679e20ab1819eca04
Matteo Nespoli <matteo1.nespoli@mail.polimi.it>
2020-05-16 11:49:05 +0200

    added SingleDivinityChoicePage, SingleDivinityChoice.css, SingleDivinityChoice.fxml

--- src/main/java/it/polimi/ingsw/PSP19/Client/GUI/Client.java
+++ src/main/java/it/polimi/ingsw/PSP19/Client/GUI/Client.java
@@ -39,7 +39,7 @@ public class Client extends Application implements ServerObserver {
         game = new Game(0, null, false, null, new Grid(), new Grid(), null);
         mainStage = primaryStage;
 
-        root = setCurrentPage(new DivinitiesChoichePage());
+        root = setCurrentPage(new DivinitiesChoicePage());
 
         Scene scene = new Scene(root, width, height);
 
